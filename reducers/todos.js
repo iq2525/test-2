b12ee054344efd9a105e2c891606ec0c3e381601
i.ahmed <iqbal.ahmed@net-a-porter.com>
2016-01-16 19:06:49 +0000

    Cleaned up reducer

--- reducers/todos.js
+++ reducers/todos.js
@@ -1,24 +1,19 @@
 import { SELECTED_RPS_TYPE } from '../constants/ActionTypes'
 
 const initialState = {
-    toDos: [
-      {
-        text: 'Use Redux',
-        completed: false,
-        id: 0
-      }
-    ],
     rpsType: null
 }
 
 
 export default function todos(state = initialState, action) {
+  console.log('REDUCER - state:', state);
+
   switch (action.type) {
     case SELECTED_RPS_TYPE:
-      console.log('todos - state:', state);
-      let toDos = state.toDos;
 
-      let stateObject = Object.assign({}, state, { toDos: toDos});
+      //let toDos = state.toDos;
+
+      let stateObject = Object.assign({}, state);
       stateObject.rpsType = action.rpsType;
 
       console.log('stateObject:', stateObject);
